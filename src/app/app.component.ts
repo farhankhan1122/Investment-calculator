a20be3e16c1f32d0dd657005e20b09977928bd9a
Farhan Khan <kfarhan634@MacBook-Air.local>
2025-03-12 04:18:19 +0530

    migrated to module based architecture

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,9 @@
 import { Component, signal } from '@angular/core';
-import { HeaderComponent } from "./header/header.component";
-import { UserInputComponent } from "./user-input/user-input.component";
-import { InvestmentResultsComponent } from "./investment-results/investment-results.component";
-import { InvestmentInput } from './investment-input.model';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, UserInputComponent, InvestmentResultsComponent],
   templateUrl: './app.component.html',
+  standalone: false,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
